2565afeafb90820140fdf89d0ea9bedb374aca7b
unknown <mcjannetp@gmail.com>
2025-01-19 17:35:43 -0500

    added read me file

---
+++ README.md
@@ -0,0 +1,22 @@
+# Python Number Guessing Game (v1.0)
+
+## About this project
+
+This project was made for a Python course. It generates a random number and it
+will let users try to guess that number, telling them if their guess is higher,
+lower, or correct. The game allows users to select from three levels, each with
+their own unique number ranges. This game makes use of critical Python concepts
+like functions, loops and conditionals.
+
+## Improvements and Optimization
+
+In the future I would like to add a leaderboard to this program that tracks the
+best guessers (those that can guess the number in the least amount of tries) and
+those that can win in the lest amount of time. I would like to make this a
+Django project so that I can maintain this data on a backend.
+
+## Author
+
+-    My Website: [paulmcjannet.com](https://www.paulmcjannet.com/)
+-    LinkedIn: [Paul McJannet](https://www.linkedin.com/in/paul-mcjannet/)
+-    Github Profile: [paul-mcj](https://github.com/paul-mcj/)
--- final_game.py
+++ final_game.py
@@ -1,4 +1,5 @@
 # Paul McJannet | Student no. 41171723 | Python Game
+# TODO: add timer, results, leaderboard, sign in/up...
 
 # import modules
 import random
@@ -6,6 +7,9 @@ from sys import exit
 
 # text output explaining how the game works
 print("\n***** Hello, and Welcome to the Number Guessing Game!*****\n\n~~~Rules~~~\n-- You need to correctly guess a random number.\n-- This starts by selecting your level (1, 2 or 3) which will change the range of possible numbers to guess from.\n-- After each guess, you will be told if the random number is higher or lower than your most recent guess.\n\n~~~Options~~~\nAt any time:\n-- Type 'q' then 'ENTER' to quit the program\n\n~~~Levels~~~\n-- Type '1' to guess from 1 - 10\n-- Type '2' to guess from 1 - 100\n-- Type '3' to guess from 1 - 1000")
+
+ # text output explaining how the game works
+    # print("\n***** Hello, and Welcome to the Number Guessing Game!*****\n\n~~~Rules~~~\n-- You need to correctly guess a random number.\n-- This starts by selecting your level (1, 2 or 3) which will change the range of possible numbers to guess from.\n-- After each guess, you will be told if the random number is higher or lower than your most recent guess.\n-- Once you guess correctly, the game timer will end.\n-- If you beat either a high score (least guesses needed to guess correct) or the timer (fastest time to guess correct) you might just get lucky enough to add your name to the Top 10 Leaderboard!\n\n~~~Options~~~\nAt any time:\n-- Type 'q' then 'ENTER' to quit the program\n-- Type 'leaderboard' then 'ENTER' to see all high scores and fastest times (note: this will not stop the timer if already in a game)\n\n~~~Level Selection~~~\nTo being, please select a level:\n-- Type '1' to guess from 1 - 10\n-- Type '2' to guess from 1 - 100\n-- Type '3' to guess from 1 - 1000")
 
 # global variable needed to set range of numbers (depending on level selection, this is used inside functions with different scope)
 set_upper_range = 0
@@ -26,6 +30,8 @@ def main_game_loop(range):
             if convert_guess <= range and convert_guess > 0:
                 if convert_guess == rand_number:
                     print(f"\n************************************************************************\nCongratulations! Your guess is correct and the number was {rand_number}!\n************************************************************************")
+                     # TODO: if top 10 met in time or number of guesses ask for username (which eventually says thanks for playing and quits program)
+                        # TODO: otherwise, say thanks for playing and quit program
                     break
                 elif rand_number > convert_guess:
                     print(f"The number is greater than {convert_guess}, try again.\n")
@@ -72,21 +78,45 @@ def prompt_level_selection_fn():
             break
 
         # user wants to quit program
+            # TODO: stop timer and quit everything
         elif prompt_level == "q":
             print(f"\nThank-you for playing! See you next time!")
             exit()    
+            #   elif:
+            # # TODO: show leaderboard
+            # print("show leaderboard....")
+            # return False
         
         # not valid input
         else:
             print(f"Not a valid level selection, try again.\n")
             continue
 
-# begin level selection process
-prompt_level_selection_fn()
+if __name__ == "__main__":
+    # begin level selection process
+    prompt_level_selection_fn()
 
-# dictionary to help define level range chosen (helps with out message to user)
-level_match_value = {10: 1, 100: 2, 1000:3}
+    # dictionary to help define level range chosen (helps with out message to user)
+    level_match_value = {10: 1, 100: 2, 1000:3}
 
-# start game
-print(f"\nYou have selected level {level_match_value[set_upper_range]}.\n")
-main_game_loop(set_upper_range)
+    # start game
+    print(f"\nYou have selected level {level_match_value[set_upper_range]}.\n")
+    main_game_loop(set_upper_range)
+
+
+
+    # TODO: use secrets module?
+        # TODO: use React for front end
+        # TODO: function to show leaderboard, enter username, add to database!!
+        # TODO: add AI model like tensorflow to predict average times, show data visualization of completionists, etc, etc. 
+        # TODO: use python web services
+        # TODO: status function that shows current number of guesses and the current range of numbers 
+        # TODO: count how many guesses it took
+        # TODO: add color to the terminal, and fix its rows and columns
+        # https://pypi.org/project/tabulate/
+        # https://github.com/Textualize/rich?tab=readme-ov-file
+
+        # TODO: add timer
+        # TODO: save data for leaderboard to a database?
+        # TODO: modularize code?
+        # .format() method on strings to make pretty with curly braces {} {} {}
